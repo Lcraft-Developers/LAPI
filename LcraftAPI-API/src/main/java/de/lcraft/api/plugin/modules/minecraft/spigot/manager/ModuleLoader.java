aee6f008d265870daf07d353b542df8c817778c2
LPDMinecraft <l_jost@online.de>
2021-11-09 20:21:08 +0100

    Updated ModuleSystem
    - Updated ClassLoader
    - Updated ModuleSystem

--- LcraftAPI-API/src/main/java/de/lcraft/api/plugin/modules/minecraft/spigot/manager/ModuleLoader.java
+++ LcraftAPI-API/src/main/java/de/lcraft/api/plugin/modules/minecraft/spigot/manager/ModuleLoader.java
@@ -1,11 +1,5 @@
 package de.lcraft.api.plugin.modules.minecraft.spigot.manager;
 
-import de.lcraft.api.plugin.modules.minecraft.spigot.manager.classloader.ClassLoader;
-
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 public class ModuleLoader {
 
     private ModuleManager moduleManager;
@@ -18,14 +12,6 @@ public class ModuleLoader {
         getModuleManager().getModules().add(module);
 
         // TODO: Add the file from module to the classpath
-        URL[] urls = new URL[1];
-        try {
-            urls[0] = new File("lmodules/").toURL();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        ClassLoader classLoader = new ClassLoader(urls);
-        classLoader.addFile(module.getMainFile());
     }
 
     public ModuleManager getModuleManager() {
