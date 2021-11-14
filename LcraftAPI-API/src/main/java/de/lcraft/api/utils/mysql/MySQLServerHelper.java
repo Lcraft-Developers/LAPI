bcf022c0adf13be0a7ff0ddc90237c5fffaf040d
LPDMinecraft <l_jost@online.de>
2021-11-14 21:21:10 +0100

    Updates MySQLHelper

--- LcraftAPI-API/src/main/java/de/lcraft/api/utils/mysql/MySQLServerHelper.java
+++ LcraftAPI-API/src/main/java/de/lcraft/api/utils/mysql/MySQLServerHelper.java
@@ -2,6 +2,7 @@ package de.lcraft.api.utils.mysql;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MySQLServerHelper {
 
@@ -13,6 +14,18 @@ public class MySQLServerHelper {
 
     public PreparedStatement makeStatement(String sql) throws SQLException {
         return server.getConnection().prepareStatement(sql);
+    }
+    public boolean executeUpdate(String query) {
+        boolean flag = false;
+        try (Statement statement = getServer().getConnection().createStatement()) {
+            if (statement != null) {
+                statement.executeUpdate(query);
+                flag = true;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return flag;
     }
 
     public MySQLServer getServer() {
