1762f8347eea528472bdb4b24f76f9775541ec40
LPDMinecraft <l_jost@online.de>
2021-11-19 12:37:34 +0100

    Tested new Updates

--- TestServer/Move Plugin Files.bat
+++ TestServer/Move Plugin Files.bat
@@ -9,7 +9,7 @@ del ".\Spigot\lmodules\LcraftAPI.spigot.test-1.0.0.jar"
 
 
 echo Copy Spigot Versions
-copy "..\LcraftAPI-API\target\LcraftAPI.API-1.0.2.jar" ".\Spigot\plugins\"
+copy "..\LcraftAPI-API\target\LcraftAPI-API-1.0.2.jar" ".\Spigot\plugins\"
 
 copy "..\LcraftAPI-Spigot-TestPlugin\target\LcraftAPI.spigot.test-1.0.0.jar" ".\Spigot\lmodules\"
 
@@ -18,15 +18,15 @@ copy "..\LcraftAPI-Spigot-TestPlugin\target\LcraftAPI.spigot.test-1.0.0.jar" ".\
 
 
 echo Delete Bungee Versions
-del ".\BungeeCord\plugins\LcraftAPI.API-1.0.2.jar"
+del ".\BungeeCord\plugins\LcraftAPI-API-1.0.2.jar"
 
-del ".\BungeeCord\lmodules\LcraftAPI.bungeecord.test-1.0.0.jar"
+del ".\BungeeCord\lmodules\LcraftAPI-bungeecord.test-1.0.0.jar"
 
 
 echo Copy Bungee Versions
-copy "..\LcraftAPI-API\target\LcraftAPI.API-1.0.2.jar" ".\BungeeCord\plugins\"
+copy "..\LcraftAPI-API\target\LcraftAPI-API-1.0.2.jar" ".\BungeeCord\plugins\"
 
-copy "..\Lcraft-BungeeCord-TestPlugin\target\LcraftAPI.bungeecord.test-1.0.0.jar" ".\BungeeCord\lmodules\"
+copy "..\Lcraft-BungeeCord-TestPlugin\target\LcraftAPI-bungeecord.test-1.0.0.jar" ".\BungeeCord\lmodules\"
 
 pause
 goto moving
