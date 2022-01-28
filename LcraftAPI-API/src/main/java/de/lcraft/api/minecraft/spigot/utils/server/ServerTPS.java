f85080df879596b993e669e4d768a906b3cff8cd
LPDMinecraft <l_jost@online.de>
2022-01-28 12:44:30 +0100

    Updatet GitHub Pages

--- LcraftAPI-API/src/main/java/de/lcraft/api/minecraft/spigot/utils/server/ServerTPS.java
+++ LcraftAPI-API/src/main/java/de/lcraft/api/minecraft/spigot/utils/server/ServerTPS.java
@@ -13,15 +13,15 @@ public class ServerTPS {
 	}
 
 	public class Lag implements Runnable {
-		public static int TICK_COUNT = 0;
-		public static long[] TICKS = new long[600];
-		public static long LAST_TICK = 0L;
+		public int TICK_COUNT = 0;
+		public long[] TICKS = new long[600];
+		public long LAST_TICK = 0L;
 
-		public static double getTPS() {
+		public double getTPS() {
 			return getTPS(100);
 		}
 
-		public static double getTPS(int ticks) {
+		public double getTPS(int ticks) {
 			if (TICK_COUNT < ticks) {
 				return 20.0D;
 			}
@@ -31,7 +31,7 @@ public class ServerTPS {
 			return ticks / (elapsed / 1000.0D);
 		}
 
-		public static long getElapsed(int tickID) {
+		public long getElapsed(int tickID) {
 			if (TICK_COUNT - tickID >= TICKS.length) {
 			}
 
