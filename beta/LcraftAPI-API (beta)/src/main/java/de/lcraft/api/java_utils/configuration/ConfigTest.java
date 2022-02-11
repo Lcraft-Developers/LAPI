b82572291ac243ec4b5326af05256b327b6e4c89
LPDMinecraft <l_jost@online.de>
2022-02-11 13:43:26 +0100

    Renew LcraftAPI (Beta) - Part 36/3
    - Updated YAMLConfigFileWriter
    - Updated Test class
    
    Do not use YAMLConfigFileWriter, because its not done

--- beta/LcraftAPI-API (beta)/src/main/java/de/lcraft/api/java_utils/configuration/ConfigTest.java
+++ beta/LcraftAPI-API (beta)/src/main/java/de/lcraft/api/java_utils/configuration/ConfigTest.java
@@ -8,14 +8,14 @@ import de.lcraft.api.java_utils.configuration.writer.YAMLConfigFileWriter;
 public class ConfigTest {
 
 	public static void main(String[] args) {
-		Config cfg1 = new Config("test.yml", new EasyConfigFileWriter());
+		Config cfg1 = new Config("testEasy.yml", new EasyConfigFileWriter());
 		cfg1.load();
 		cfg1.set("test.lpd", 2);
 		cfg1.set("test", 1);
 		cfg1.set("test.lpd.nice", "Hallo");
 		cfg1.save();
 
-		Config cfg2 = new Config("test.yml", new EasyConfigFileWriter());
+		Config cfg2 = new Config("testEasy.yml", new EasyConfigFileWriter());
 		cfg2.load();
 		for(ConfigSection c : cfg2.getAllConfigurationSections()) {
 			if(c.getConfigSectionType() == ConfigSectionType.OnlyValue || c.getConfigSectionType() == ConfigSectionType.OnlyValue) {
@@ -31,14 +31,14 @@ public class ConfigTest {
 		}
 
 
-		Config cfg3 = new Config("test.yml", new YAMLConfigFileWriter());
+		Config cfg3 = new Config("testYaml.yml", new YAMLConfigFileWriter());
 		cfg3.load();
 		cfg3.set("test.lpd", 2);
 		cfg3.set("test", 1);
 		cfg3.set("test.lpd.nice", "Hallo");
 		cfg3.save();
 
-		Config cfg4 = new Config("test.yml", new YAMLConfigFileWriter());
+		Config cfg4 = new Config("testYaml.yml", new YAMLConfigFileWriter());
 		cfg4.load();
 		for(ConfigSection c : cfg4.getAllConfigurationSections()) {
 			if(c.getConfigSectionType() == ConfigSectionType.OnlyValue || c.getConfigSectionType() == ConfigSectionType.OnlyValue) {
