7b0b81f47e7ea8082790b11bd896d09a716536d2
LPDMinecraft <l_jost@online.de>
2022-02-26 10:03:49 +0100

    Renew LcraftAPI (Beta) - Part 40
    - Bug fixed code
    - Updated website
    
    Next plans:
    - WorldEditor System Part 2
    - WorldGenerator System Part 1
    
    Future plans:
    - Item Update (make easier)
    - MobCreater (make code)
    
    Co-Authored-By: Lukas006 <85806442+Lukas006@users.noreply.github.com>

--- beta/LcraftAPI-API (beta)/src/main/java/de/lcraft/api/minecraft/spigot/module/manager/command/SubCommand.java
+++ beta/LcraftAPI-API (beta)/src/main/java/de/lcraft/api/minecraft/spigot/module/manager/command/SubCommand.java
@@ -1,5 +1,6 @@
 package de.lcraft.api.minecraft.spigot.module.manager.command;
 
+import de.lcraft.api.java_utils.language.LanguagesManager;
 import de.lcraft.api.minecraft.spigot.module.manager.utils.language.StandardMessages;
 import de.lcraft.api.minecraft.spigot.module.manager.utils.listeners.ListenerManager;
 import de.lcraft.api.minecraft.spigot.module.manager.utils.permissions.PermsManager;
@@ -7,8 +8,8 @@ import de.lcraft.api.minecraft.spigot.module.player.LPlayerManager;
 
 public abstract class SubCommand extends Command {
 
-	public SubCommand(StandardMessages standardMessages, String subLabel, String desc, PermsManager permsManager, LPlayerManager lPlayerManager, LanguagesManager languagesManager, boolean splitting, ListenerManager listenerManager) {
-		super(standardMessages, subLabel, desc, permsManager, lPlayerManager, languagesManager, splitting, listenerManager);
+	public SubCommand(StandardMessages standardMessages, String subLabel, String desc, PermsManager permsManager, LPlayerManager lPlayerManager, LanguagesManager languagesManager, boolean splitting) {
+		super(standardMessages, subLabel, desc, permsManager, languagesManager, lPlayerManager, splitting);
 	}
 
 }
