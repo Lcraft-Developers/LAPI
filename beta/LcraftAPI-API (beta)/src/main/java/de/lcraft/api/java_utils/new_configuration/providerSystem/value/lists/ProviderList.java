6ab0195635eb74dd6d13ee19e3fd693a1645f3d1
LPDMinecraft <l_jost@online.de>
2022-04-22 15:21:23 +0200

    LcraftAPI (Beta) - Part 44/12
    - Updated Provider System
    
    Next plans:
    - Add own ListenerSystem
    - Transfer all BukkitEvents to LcraftEvents
    - Add a LcraftEventTransformation class to handle the events
    - Finish new LcraftListenerSystem
    
    Future plans:
    - Item Update (make easier and faster to access) (Add more functions to Builders)
    - Inventory Update (make easier and faster to access) (Remove InventoryConsumerItem)
    - Command Update (make a new system for commands and subCommands)
    
    - Finish all ConfigSystem plans
    - Made MobBuilder
    - Holo system
    - Make Encryption system
    - WorldEditor System Part 2
    - WorldGenerator System Part 1
    - Finish all PermissionSystem plans
    
    Our ConfigSystem plans:
    - Add Provider System
    - Add YAML Provider System
    - Add JSON Provider System
    - Add Comments to the YAML System
    - Add local DB Provider System
    - Add MySQL Provider System
    - Test ConfigSystem
    - Add ConfigFileWriter type for compact, encrypted and unreadable files
    
    Our PermissionSystem plans:
    - Create own ConfigurationSystem with the new type of ConfigFileWriter
    - Add system to save, load and request permissions
    - Add system to get all permissions from spigot, bukkit and luckperms side
    - Add website connections and mysql transformations to configure all Permissions online
    - Make everything work

--- beta/LcraftAPI-API (beta)/src/main/java/de/lcraft/api/java_utils/new_configuration/providerSystem/value/lists/ProviderList.java
+++ beta/LcraftAPI-API (beta)/src/main/java/de/lcraft/api/java_utils/new_configuration/providerSystem/value/lists/ProviderList.java
@@ -1,6 +1,7 @@
 package de.lcraft.api.java_utils.new_configuration.providerSystem.value.lists;
 
-import de.lcraft.api.java_utils.new_configuration.providerSystem.value.ProviderObject;
+import de.lcraft.api.java_utils.new_configuration.providerSystem.value.ProviderString;
 
-public abstract class ProviderList extends ProviderObject {
+public abstract class ProviderList extends ProviderString
+
 }
--- beta/LcraftAPI-API (beta)/src/main/java/de/lcraft/api/java_utils/new_configuration/providerSystem/value/lists/ProviderMap.java
+++ beta/LcraftAPI-API (beta)/src/main/java/de/lcraft/api/java_utils/new_configuration/providerSystem/value/lists/ProviderMap.java
@@ -1,6 +1,6 @@
 package de.lcraft.api.java_utils.new_configuration.providerSystem.value.lists;
 
-import de.lcraft.api.java_utils.new_configuration.providerSystem.value.ProviderObject;
+import de.lcraft.api.java_utils.new_configuration.providerSystem.value.ProviderString;
 
-public abstract class ProviderMap extends ProviderObject {
+public abstract class ProviderMap extends ProviderString {
 }
