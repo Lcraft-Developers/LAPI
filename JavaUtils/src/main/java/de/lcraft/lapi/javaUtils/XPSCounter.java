3d9aac55a04fe35686a06ec9641be42e952586c7
LPDMinecraft <l_jost@online.de>
2023-04-24 21:06:22 +0200

    JavaUtils 1.0.0 b5
    - Edited all pom.xml
    - Added test support
    - Added XPSCounter (for example FPSCounter) class
    - Packaged JavaUtils Project

---
+++ JavaUtils/src/main/java/de/lcraft/lapi/javaUtils/XPSCounter.java
@@ -0,0 +1,94 @@
+package de.lcraft.lapi.javaUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class XPSCounter {
+
+	private Long lastFrameTime;
+	private List<Long> frameTimes;
+	private Long maxNanoSeconds;
+
+	public XPSCounter(Long maxNanoSeconds) {
+		init(maxNanoSeconds);
+	}
+	public XPSCounter() {
+		init(null);
+	}
+
+	private void init(Long maxNanoSeconds) {
+		setFrameTimes(new ArrayList<>());
+		setLastFrameTime(0L);
+
+		if(maxNanoSeconds != null) setMaxNanoSeconds(maxNanoSeconds);
+		else setMaxNanoSeconds(TimeUnit.MILLISECONDS.toNanos(1000));
+	}
+
+	public void nextFrame() {
+		addFrameTime(System.nanoTime());
+	}
+	public Long getAverageFrameTime_ns() {
+		long averageFrameTime = 0L;
+
+		for(Long frameTime : getFrameTimes()) {
+			averageFrameTime = averageFrameTime + ((frameTime) / getFrameTimes().size());
+		}
+
+		return averageFrameTime;
+	}
+	public Long getAverageFrameTime_ms() {
+		return TimeUnit.NANOSECONDS.toMillis(getAverageFrameTime_ns());
+	}
+	public Long getAverageFrameTime_sec() {
+		return TimeUnit.NANOSECONDS.toSeconds(getAverageFrameTime_ns());
+	}
+	public Long getAverageFPS() {
+		long fps = 1000 / getAverageFrameTime_ms();
+		if(fps < 0) fps = 0L;
+		return fps;
+	}
+
+	private void addFrameTime(Long frameSystemNanoTime) {
+		refreshFrameTimes();
+		long frameTime;
+
+		if(getLastFrameTime() == 0) frameTime = 0L;
+		else frameTime = frameSystemNanoTime - getLastFrameTime();
+
+		getFrameTimes().add(frameTime);
+		setLastFrameTime(frameSystemNanoTime);
+	}
+	private void refreshFrameTimes() {
+		List<Long> newFrameTimes = new ArrayList<>();
+
+		for(Long frameTime_ns : getFrameTimes()) {
+			if(frameTime_ns <= getMaxNanoSeconds()) {
+				newFrameTimes.add(frameTime_ns);
+			}
+		}
+
+		setFrameTimes(newFrameTimes);
+	}
+
+	public Long getLastFrameTime() {
+		return lastFrameTime;
+	}
+	public List<Long> getFrameTimes() {
+		return frameTimes;
+	}
+	public Long getMaxNanoSeconds() {
+		return maxNanoSeconds;
+	}
+
+	private void setLastFrameTime(Long lastFrameTime) {
+		this.lastFrameTime = lastFrameTime;
+	}
+	private void setFrameTimes(List<Long> frameTimes) {
+		this.frameTimes = frameTimes;
+	}
+	private void setMaxNanoSeconds(Long maxNanoSeconds) {
+		this.maxNanoSeconds = maxNanoSeconds;
+	}
+
+}
---
+++ JavaUtils/src/test/java/de/lcraft/lapi/javaUtils/XPSCounterTest.java
@@ -0,0 +1,59 @@
+package de.lcraft.lapi.javaUtils;
+
+import org.junit.jupiter.api.Test;
+import java.util.concurrent.TimeUnit;
+
+public class XPSCounterTest {
+
+	@Test
+	public void test() {
+		int repeats = 3;
+
+		int[] framerates = {
+				30, 60, 90,
+				144, 300
+		};
+		long f = 0L;
+		int maxScore = 0;
+
+		for(int fps : framerates) {
+			maxScore = maxScore + fps * repeats;
+		}
+
+		for(int msDuration = 1000; msDuration <= (1000 * repeats); msDuration = msDuration + 1000) {
+			for(int fps : framerates) {
+				try {
+					f = f + makeTest(fps, msDuration);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+
+		System.out.println(System.lineSeparator() + "Score: " + f);
+		System.out.println("Max Score: " + maxScore);
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		makeTest(144, 5000);
+	}
+
+	public static Long makeTest(int fps, int msDuration) throws InterruptedException {
+		XPSCounter xpsCounter = new XPSCounter(TimeUnit.MILLISECONDS.toNanos(msDuration));
+		int msLength = 1000 / fps;
+
+		System.out.println(System.lineSeparator() + "Testing...");
+		System.out.println(System.lineSeparator() + "Time Duration: " + msDuration + "ms");
+		System.out.println("Expected result: " + fps + "FPS");
+
+		for(int i = 0; i < msDuration; i = i + msLength) {
+			Thread.sleep(msLength);
+			xpsCounter.nextFrame();
+		}
+		Long avgFPS = xpsCounter.getAverageFPS();
+
+		System.out.println("Results: " + avgFPS + "FPS");
+		return avgFPS;
+	}
+
+}
